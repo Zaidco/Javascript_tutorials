6edd02f94c892c0ccd4488a04638f9b2bdb2192e
Zaid <143437881+Zaidco@users.noreply.github.com>
2025-05-04 17:50:38 +0000

    datatypes-conversion

---
+++ 01_basics/conversion_confusion.js
@@ -0,0 +1,22 @@
+let score =33;
+
+let new_score= String(score) //this way we can define any variable for a particular datatype. Yaha pe score ek number hai, still it appeared as a String-----------------------------
+
+console.log(typeof score); //a method to know the type of datatype
+console.log(typeof (score));// just another method
+
+console.log(score);
+console.log(typeof new_score);
+
+
+let isLoggedin = "hello"
+//let isLoggedinn = hello  // this is not allowed since a string must be under " "
+let booleanisLoggedin  = Boolean(isLoggedin)
+
+console.log(typeof isLoggedin);
+console.log(booleanisLoggedin);
+
+
+
+
+
