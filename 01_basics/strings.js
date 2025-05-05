eaecb3b9345b3f4a55cc3f32a43d5f6d17b7f7d9
Zaid <143437881+Zaidco@users.noreply.github.com>
2025-05-05 18:15:00 +0000

    strings

---
+++ 01_basics/strings.js
@@ -0,0 +1,37 @@
+const name= "Zaid"
+const repoCount=50
+let repoContribute= 25
+
+console.log(name+repoContribute+repoCount);
+console.log(name+repoContribute+repoCount+ 35+ 'hello'+"isthiscorrect");
+
+console.log(25);
+console.log(repoContribute);
+
+
+//another way to print string
+console.log(`my name is ${name} and my repocount is ${repoCount} , and also my contribution is ${repoContribute}`); // follow this method
+
+
+//some more operations on strings-
+const newString= new String("My Game Name")
+
+console.log(newString.charAt(3));
+
+console.log(newString.indexOf('a'));
+
+console.log(newString.charAt(3+1));
+
+const NewGame= newString.substring(0,5)
+console.log(NewGame);
+
+
+
+
+ 
+
+
+
+
+
+
